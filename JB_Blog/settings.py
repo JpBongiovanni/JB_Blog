81fe8eefdb1e57d0d572621124575f6d3358a70c
Jason Bongiovanni <jpbongiovanni@gmail.com>
2022-02-17 20:41:19 -0500

    removed sentry

--- JB_Blog/settings.py
+++ JB_Blog/settings.py
@@ -149,19 +149,3 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
 
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
-
-sentry_sdk.init(
-    dsn="https://def9b32d939e43edbe7005c0836eb525@o1146879.ingest.sentry.io/6216485",
-    integrations=[DjangoIntegration()],
-
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
-    traces_sample_rate=1.0,
-
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True
-)
